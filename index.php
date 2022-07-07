1746885d08895109205fbf1515a57dfba93f3586
gtpesantren22 <cakromat26@gmail.com>
2022-07-08 03:21:13 +0700

    Update halaman maintenance

--- index.php
+++ index.php
@@ -11,7 +11,41 @@
 </head>
 
 <body>
-    <center><img src="SIMKU.jpg" alt="" height="100%"></center>
+    <center>
+        <h2>Coming Soon at <b id="demo"></b></h2>
+        <img src="SIMKU1.jpg" alt="">
+    </center>
 </body>
+
+<script>
+    // Mengatur waktu akhir perhitungan mundur
+    var countDownDate = new Date("Jul 11, 2022 00:00:00").getTime();
+
+    // Memperbarui hitungan mundur setiap 1 detik
+    var x = setInterval(function() {
+
+        // Untuk mendapatkan tanggal dan waktu hari ini
+        var now = new Date().getTime();
+
+        // Temukan jarak antara sekarang dan tanggal hitung mundur
+        var distance = countDownDate - now;
+
+        // Perhitungan waktu untuk hari, jam, menit dan detik
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Keluarkan hasil dalam elemen dengan id = "demo"
+        document.getElementById("demo").innerHTML = days + " hari, " + hours + ":" +
+            minutes + ":" + seconds + " ";
+
+        // Jika hitungan mundur selesai, tulis beberapa teks 
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("demo").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+</script>
 
 </html>
