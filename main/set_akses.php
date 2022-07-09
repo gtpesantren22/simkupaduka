854dadb25856481724722d48b301460c4de717cb
gtpesantren22 <cakromat26@gmail.com>
2022-07-10 01:11:33 +0700

    Update pilih lembaga hak akses

--- main/set_akses.php
+++ main/set_akses.php
@@ -50,7 +50,7 @@ include 'head.php';
                                         <tbody>
                                             <?php
                                             $no = 1;
-                                            $dt_bos = mysqli_query($conn, "SELECT a.*, b.nama FROM akses a JOIN lembaga b ON a.lembaga=b.kode WHERE a.tahun = '$tahun_ajaran' ");
+                                            $dt_bos = mysqli_query($conn, "SELECT a.*, b.nama FROM akses a JOIN lembaga b ON a.lembaga=b.kode WHERE a.tahun = '$tahun_ajaran' AND b.tahun = '$tahun_ajaran' ");
                                             while ($a = mysqli_fetch_assoc($dt_bos)) { ?>
                                                 <tr>
                                                     <td><?= $no++ ?></td>
