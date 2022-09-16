c66cab4420661340f91342bb06ede7ac98de5f7e
gtpesantren22 <cakromat26@gmail.com>
2022-09-16 13:28:08 +0700

    Update info spj di accounting

--- account/spj.php
+++ account/spj.php
@@ -2,16 +2,16 @@
 include 'head.php';
 ?>
 <!-- Datatables -->
-<link href="vendors/datatables.net-bs/css/dataTables.bootstrap.min.css" rel="stylesheet">
-<link href="vendors/datatables.net-buttons-bs/css/buttons.bootstrap.min.css" rel="stylesheet">
-<link href="vendors/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css" rel="stylesheet">
-<link href="vendors/datatables.net-responsive-bs/css/responsive.bootstrap.min.css" rel="stylesheet">
-<link href="vendors/datatables.net-scroller-bs/css/scroller.bootstrap.min.css" rel="stylesheet">
+<link href="../main/vendors/datatables.net-bs/css/dataTables.bootstrap.min.css" rel="stylesheet">
+<link href="../main/vendors/datatables.net-buttons-bs/css/buttons.bootstrap.min.css" rel="stylesheet">
+<link href="../main/vendors/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css" rel="stylesheet">
+<link href="../main/vendors/datatables.net-responsive-bs/css/responsive.bootstrap.min.css" rel="stylesheet">
+<link href="../main/vendors/datatables.net-scroller-bs/css/scroller.bootstrap.min.css" rel="stylesheet">
 
 <!-- bootstrap-daterangepicker -->
-<link href="vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+<link href="../main/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
 <!-- bootstrap-datetimepicker -->
-<link href="vendors/bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css" rel="stylesheet">
+<link href="../main/vendors/bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css" rel="stylesheet">
 
 <!-- page content -->
 <div class="right_col" role="main">
@@ -321,23 +321,23 @@ include 'head.php';
 
 <?php include 'foot.php'; ?>
 <!-- Datatables -->
-<script src="vendors/datatables.net/js/jquery.dataTables.min.js"></script>
-<script src="vendors/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
-<script src="vendors/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
-<script src="vendors/datatables.net-buttons-bs/js/buttons.bootstrap.min.js"></script>
-<script src="vendors/datatables.net-buttons/js/buttons.flash.min.js"></script>
-<script src="vendors/datatables.net-buttons/js/buttons.html5.min.js"></script>
-<script src="vendors/datatables.net-buttons/js/buttons.print.min.js"></script>
-<script src="vendors/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js"></script>
-<script src="vendors/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
-<script src="vendors/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
-<script src="vendors/datatables.net-responsive-bs/js/responsive.bootstrap.js"></script>
-<script src="vendors/datatables.net-scroller/js/dataTables.scroller.min.js"></script>
+<script src="../main/vendors/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="../main/vendors/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+<script src="../main/vendors/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+<script src="../main/vendors/datatables.net-buttons-bs/js/buttons.bootstrap.min.js"></script>
+<script src="../main/vendors/datatables.net-buttons/js/buttons.flash.min.js"></script>
+<script src="../main/vendors/datatables.net-buttons/js/buttons.html5.min.js"></script>
+<script src="../main/vendors/datatables.net-buttons/js/buttons.print.min.js"></script>
+<script src="../main/vendors/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js"></script>
+<script src="../main/vendors/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
+<script src="../main/vendors/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+<script src="../main/vendors/datatables.net-responsive-bs/js/responsive.bootstrap.js"></script>
+<script src="../main/vendors/datatables.net-scroller/js/dataTables.scroller.min.js"></script>
 <!-- bootstrap-daterangepicker -->
-<script src="vendors/moment/min/moment.min.js"></script>
-<script src="vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+<script src="../main/vendors/moment/min/moment.min.js"></script>
+<script src="../main/vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
 <!-- bootstrap-datetimepicker -->
-<script src="vendors/bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
+<script src="../main/vendors/bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
 <script>
     $(document).ready(function() {
         $('#datatable2').DataTable();
