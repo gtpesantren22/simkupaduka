78a54b7498970ddafabfca2800f40a886dc11209
gtpesantren22 <cakromat26@gmail.com>
2023-07-02 10:51:05 +0700

    tambah kode rencana bulan

--- application/views/lembaga/rab24.php
+++ application/views/lembaga/rab24.php
@@ -375,9 +375,18 @@
                         <div class="col-md-6 col-sm-6 ">
                             <select name="rencana" id="" required class="form-control">
                                 <option value=""> -- pilih waktu -- </option>
-                                <option value="Semester 1"> Semester 1 </option>
-                                <option value="Semester 2"> Semester 2 </option>
-                                <option value="Semester 1&2"> Semester 1&2 </option>
+                                <option value="1"> Januari </option>
+                                <option value="2"> Februari </option>
+                                <option value="3"> Maret </option>
+                                <option value="4"> April </option>
+                                <option value="5"> Mei </option>
+                                <option value="6"> Juni </option>
+                                <option value="7"> Juli </option>
+                                <option value="8"> Agustus </option>
+                                <option value="9"> September </option>
+                                <option value="10"> Oktober </option>
+                                <option value="11"> November </option>
+                                <option value="12"> Desember </option>
                             </select>
                         </div>
                     </div>
