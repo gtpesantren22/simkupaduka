c6e6b5fb33fae7c67c49879dcaa51b567d84e3e8
gtpesantren22 <cakromat26@gmail.com>
2023-07-22 05:22:48 +0700

    update upload spj lembaga

--- application/controllers/Lembaga.php
+++ application/controllers/Lembaga.php
@@ -548,8 +548,8 @@ Terimakasih';
 
 			if ($this->db->affected_rows() > 0) {
 
-				kirim_group($$this->apiKey, '120363040973404347@g.us', $psn);
-				kirim_group($$this->apiKey, '120363042148360147@g.us', $psn);
+				kirim_group($this->apiKey, '120363040973404347@g.us', $psn);
+				kirim_group($this->apiKey, '120363042148360147@g.us', $psn);
 				// kirim_person($this->apiKey, '085236924510', $psn);
 
 				$this->session->set_flashdata('ok', 'Bukti SPJ berhasil diupload');
@@ -1571,8 +1571,8 @@ Terimakasih';
 
 			if ($this->db->affected_rows() > 0) {
 
-				// kirim_group($$this->apiKey, '120363040973404347@g.us', $psn);
-				// kirim_group($$this->apiKey, '120363042148360147@g.us', $psn);
+				// kirim_group($this->apiKey, '120363040973404347@g.us', $psn);
+				// kirim_group($this->apiKey, '120363042148360147@g.us', $psn);
 				kirim_person($this->apiKey, '085236924510', $psn);
 
 				$this->session->set_flashdata('ok', 'Bukti SPJ berhasil diupload');
