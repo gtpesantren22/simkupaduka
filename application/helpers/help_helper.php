7dd3b2d69aae31dd00a5a2fd6ecca51be466cfa7
gtpesantren22 <cakromat26@gmail.com>
2025-12-14 13:20:04 +0700

    update: non-aktifan send personal

--- application/helpers/help_helper.php
+++ application/helpers/help_helper.php
@@ -65,26 +65,26 @@ function tanggalIndo($tanggal)
     return $tanggal;
 }
 
-function kirim_person($key, $no_hp, $pesan)
-{
-    $curl2 = curl_init();
-    curl_setopt_array(
-        $curl2,
-        array(
-            CURLOPT_URL => 'http://31.97.179.141:3000/api/sendMessage',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => 'apiKey=' . $key . '&phone=' . $no_hp . '&message=' . $pesan,
-        )
-    );
-    $response = curl_exec($curl2);
-    curl_close($curl2);
-}
+// function kirim_person($key, $no_hp, $pesan)
+// {
+//     $curl2 = curl_init();
+//     curl_setopt_array(
+//         $curl2,
+//         array(
+//             CURLOPT_URL => 'http://31.97.179.141:3000/api/sendMessage',
+//             CURLOPT_RETURNTRANSFER => true,
+//             CURLOPT_ENCODING => '',
+//             CURLOPT_MAXREDIRS => 10,
+//             CURLOPT_TIMEOUT => 0,
+//             CURLOPT_FOLLOWLOCATION => true,
+//             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//             CURLOPT_CUSTOMREQUEST => 'POST',
+//             CURLOPT_POSTFIELDS => 'apiKey=' . $key . '&phone=' . $no_hp . '&message=' . $pesan,
+//         )
+//     );
+//     $response = curl_exec($curl2);
+//     curl_close($curl2);
+// }
 
 function kirim_group($key, $id_group, $pesan)
 {
